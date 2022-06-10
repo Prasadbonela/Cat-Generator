6c50ba34acb94a809ce0eb516393c1853128d7a0
Sofia Krylova <sofia_krylova@epam.com>
2022-06-10 15:50:29 +0200

    Initial

---
+++ src/main/java/com/epam/autotasks/CatGenerator.java
@@ -0,0 +1,16 @@
+package com.epam.autotasks;
+
+import java.util.List;
+
+public class CatGenerator {
+
+    public static List<Cat> generateCats(int count) {
+
+        return null;
+    }
+
+    public static long generateFood(int familySize, int skip) {
+
+        return 0;
+    }
+}
